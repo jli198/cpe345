d02fb89bb6a924057603a42baf0e7b68e4635e04
jli198 <jli198@stevens.edu>
2023-04-12 11:42:36 -0400

    publish lec13

---
+++ lec13/router.cc
@@ -0,0 +1,14 @@
+case ALG_MIN_QUEUE_LENGTH:
+  // TODO implementation missing
+  outGateIndex = -1;
+    break;
+
+case ALG_MIN_DELAY:
+  // TODO implementation missing
+  outGateIndex = -1;
+    break;
+
+case ALG_MIN_SERVICE_TIME:
+  // TODO implementation missing
+  outGateIndex = -1;
+    break;
